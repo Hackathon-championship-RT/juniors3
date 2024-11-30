054e7cb3a85b5ce76370f56d4ff8223a1bfadbdd
Karim Suleymanov <gchdjjfjfjf@mail.ru>
2024-11-30 08:36:05 +0300

    shadows and turtle

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import StartApp from '../src/components/StartApp.vue'
 import Header from "@/components/Header.vue";
 import Board from "@/components/Board.vue";
+import FigureService from "@/services/FigureService.js";
 
 export default {
   components: {
@@ -19,6 +20,9 @@ export default {
     }
   },
   methods: {
+    FigureService() {
+      return FigureService
+    },
     Choose(tile) {
 
       if (this.Choosen.src) {
@@ -51,15 +55,7 @@ export default {
     <Header/>
     <main class="" style="min-height: 100vh;">
       <Board
-          :tiles="[{x:0,y:0.5,z:0,type: 0},
-                   {x:1,y:0,z:0,type: 0},
-                   {x:1,y:0,z:1,type: 0},
-                   {x:1,y:0,z:2,type: 0},
-                   {x:2,y:0,z:0,type: 0},
-                   {x:1,y:1,z:0,type: 0},
-                   {x:2,y:1,z:0,type: 0},
-                   {x:2,y:1,z:1,type: 0},
-                   {x:3,y:0.5,z:0,type: 0}]"
+          :tiles="FigureService().turtle"
       />
     </main>
   </div>
