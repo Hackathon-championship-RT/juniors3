7279c7aebc38e84917f1e27fa84ab064c36b871a
Poco-dev <vueprojector5@gmail.com>
2024-11-30 20:20:29 +0300

    Update Header.vue

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -36,12 +36,12 @@ export default {
 <template>
   <header class="flex justify-around content-center h-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-500"
     style="z-index: 2; position: absolute; width: 100%">
-    <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('restart')">Выбрать уровень</p>
+    <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('restart')">Новая игра</p>
     <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('update')">Начать сначала</p>
     <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('reshuffle')">Перемешать</p>
     <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('showhint')">Подсказка</p>
     <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('revert')">Отмена хода</p>
-    <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('leaderBoard')">LeaderBoard</p>
+    <p class="text-white mt-1 hover:cursor-pointer" @click="$emit('leaderBoard')">Таблица лидеров</p>
     <p class="text-white mt-1 hover:cursor-pointer">Время: <span v-if="hour">{{ hour }}:</span>{{ min }}:{{ sec }}</p>
   </header>
 </template>
